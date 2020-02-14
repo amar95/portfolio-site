0ffaf93250d9f2546acb41d0b3499afa535991d1
Amarendra Sharma <s.amarendra1995@gmail.com>
2020-02-14 23:54:48 +0530

    balck and white resume

---
+++ testing.js
@@ -0,0 +1,18 @@
+class k {
+  constructor(px) {
+    this.px = px;
+  }
+  x() {
+    let oo = {};
+    let pp = { ss: "rrtyyy" };
+    let qq = ["sdsd", "oo"];
+    let ii = "asasas-aksaks-oooo";
+    ii.split("-");
+    qq.join(",");
+    console.log(ii.split("-"));
+    oo = this.px;
+    debugger;
+  }
+}
+let ppp = new k(88);
+ppp.x();
